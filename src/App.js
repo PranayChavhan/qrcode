b857fda1500b84c518158bbd78da58164fd27c8e
PranayChavhan <pranay21chavhan@outlook.com>
2022-08-24 16:25:35 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
+import LiveQrCode from "./components/live-qr-code";
+var QRCode = require("qrcode");
 function App() {
+  const [qrText, setQrText] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const generateQrCode = () => {
+    QRCode.toDataURL(
+      qrText,
+      {
+        width: 900,
+        margin: 3,
+      },
+      (err, url) => {
+        if (err) {
+          return console.log(err);
+        }
+        setQrCode(url);
+      }
+    );
+  };
+  const handleQrCode = (e) => {
+    setQrText(e.target.value);
+    generateQrCode();
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <LiveQrCode value={qrText} />
+        <label style={{ marginTop: 20 }}>Qr Code Text</label>
+        <br />
+        <input type="text" value={qrText} onChange={handleQrCode} />
+        <br />
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          style={{
+            backgroundColor: "lightblue",
+            textDecoration: "none",
+            color: "black",
+            padding: 5,
+            borderRadius: "5px",
+            marginTop: 10,
+          }}
+          href={qrCode}
+          download={`${qrText}.png`}
         >
-          Learn React
+          Download
         </a>
       </header>
     </div>
